cfe26d8b7926b2847b837558937c9376392fbe84
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2023-12-06 01:39:56 -0600

    Añadiendo variables de entorno

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit7d5eb1ba5c25fe5c5e5bb80f6f988179
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit7d5eb1ba5c25fe5c5e5bb80f6f988179::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
